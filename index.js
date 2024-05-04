59fd23863c792bced5c30c0e2f6ac409135e8781
arpit512 <tiwariarpit512@gmail.com>
2024-05-04 19:36:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// HTML - Hyper Text Markup Language
+// CSS - Cascading Style Sheet
+// JS - JavaScript
+
+// React
+// MERN  Mongo Express React Node.JS
+
+// Frontend-middlewares-Backend
+// Database
+
+
+// Database Types
+// Structured Database and Unstructred Database 
+// Structured database: Excel, google sheets, DBMS- SQL
+// Unstructured Database: MonogDB, CasandraDB etc.
+// Cloud Solutions: Google firebase, AWS, Azure
+
+//API
+
+// -- USER:
+// Record a video - upload onto youtube- 10 min Process ( ML algorithms) -- Database
+
+// -- API -- Application Programming Interface
+// -- web api - GET, Update, Delete, PUT, RETRIEVE, POST // REST, SOAP, GraphQL
+//Library API
+
+//-viewer
+// Fetch Gaming tags from database - and will show me
+
+// Download and install VS code- Install Extension (Live server, Prettier, HTML CSS support)
